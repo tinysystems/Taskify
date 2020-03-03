ace7ff05a4fe0e2025565a9a3ad29ecebb05f645
Murat Mulayim <murat.mulayim@yandex.com>
2020-03-03 22:30:58 +0300

    Add cem application and its generated code

--- Apps/bitcount.c
+++ Apps/bitcount.c
@@ -1,242 +1,298 @@
-#include <stdio.h>
-#define CHAR_BIT 8
-#define ITER 10
+// This file is part of InK.
 
-static char bits[256] = {
-        0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,  /* 0   - 15  */
-        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  /* 16  - 31  */
-        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  /* 32  - 47  */
-        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  /* 48  - 63  */
-        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  /* 64  - 79  */
-        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  /* 80  - 95  */
-        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  /* 96  - 111 */
-        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,  /* 112 - 127 */
-        1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  /* 128 - 143 */
-        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  /* 144 - 159 */
-        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  /* 160 - 175 */
-        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,  /* 176 - 191 */
-        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  /* 192 - 207 */
-        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,  /* 208 - 223 */
-        3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,  /* 224 - 239 */
-        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8   /* 240 - 255 */
-};
-static unsigned int plus = 13;
-unsigned int seed = 45697649;
-unsigned int iteration;
-unsigned int function;
-unsigned int n_0, n_1, n_2, n_3, n_4, n_5, n_6;
-
-/********************************************/
-int non_recursive_cnt(unsigned int x){
-    int cnt = bits[(int)(x & 0x0000000FL)];
-
-    while (0L != (x >>= 4)) {
-        cnt += bits[(int)(x & 0x0000000FL)];
-    }
-
-    return cnt;
-}
-/********************************************/
+/*
+ * InK is free software: you ca    n redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published *
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
-void t_bit_count() {
-    printf ("t_bit_count task \n");
-    unsigned int tmp_seed = seed;
+#include "ink.h"
+#include <stdlib.h>
 
-    seed = seed + plus;
-    unsigned temp = 0;
-    if(tmp_seed) do
-        temp++;
-    while (0 != (tmp_seed = tmp_seed&(tmp_seed-1)));
 
-    n_0 += (int)temp;
-    iteration++;
+// Define constant persistent variables that are stored in non-volatile memory.
+uint32_t bits[ 256 ] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8};
+uint32_t SEED = 4;
+uint32_t ITER = 10;
+uint32_t CHAR_BIT = 8;
+uint32_t plus = 13;
 
-    // printf("** n_0 %d \n", n_0);
-    if (iteration < ITER) {
-      t_bit_count();
-    }
-}
 
-void t_bitcount() {
-    printf ("t_bit_count task \n");
-    unsigned int tmp_seed = seed;
+// Define task-shared persistent variables.
+__shared(
+    uint32_t pinCont;
+    uint32_t n_0;
+    uint32_t n_1;
+    uint32_t n_2;
+    uint32_t n_3;
+    uint32_t n_4;
+    uint32_t n_5;
+    uint32_t n_6;
+    uint32_t function;
+    uint32_t iteration;
+    uint32_t seed;
+)
 
-    seed = seed + plus;
-    tmp_seed = ((tmp_seed & 0xAAAAAAAAL) >>  1) + (tmp_seed & 0x55555555L);
-    tmp_seed = ((tmp_seed & 0xCCCCCCCCL) >>  2) + (tmp_seed & 0x33333333L);
-    tmp_seed = ((tmp_seed & 0xF0F0F0F0L) >>  4) + (tmp_seed & 0x0F0F0F0FL);
-    tmp_seed = ((tmp_seed & 0xFF00FF00L) >>  8) + (tmp_seed & 0x00FF00FFL);
-    tmp_seed = ((tmp_seed & 0xFFFF0000L) >> 16) + (tmp_seed & 0x0000FFFFL);
 
-    n_1 += (int)tmp_seed;
-    iteration++;
+// Declare tasks that will be implemented
+ENTRY_TASK(t_init);
+TASK(t_select_func);
+TASK(t_bit_count);
+TASK(t_bitcount);
+TASK(t_ntbl_bitcnt);
+TASK(t_ntbl_bitcount);
+TASK(t_BW_btbl_bitcount);
+TASK(t_AR_btbl_bitcount);
+TASK(t_bit_shifter);
+TASK(t_end);
 
-    //printf("** n_1 %d \n", n_1);
-    if (iteration < ITER) {
-      t_bitcount();
-    }
+
+// Called at the very first boot
+void thread1_init(){
+    // create a thread with priority 15 and entry task t_init
+    __CREATE(15, t_init);
+    __SIGNAL(15);
 }
 
 
-
-
-void t_ntbl_bitcnt() {
-      printf ("t_ntbl_bitcnt task \n");
-    n_2 += non_recursive_cnt(seed);
-    seed = seed + plus;
-    //printf("** n_2 %d \n", n_2);
-    iteration++;
-
-    if (iteration < ITER) {
-      t_ntbl_bitcnt();
-    }
+// Implementation of all tasks that are declared above
+ENTRY_TASK(t_init) {
+    uint32_t _seed = rand();
+    __SET(seed, _seed);
+    __SET(seed, 45697649);
+    __SET(pinCont, 1);
+    __SET(function, 0);
+    __SET(n_0, 0);
+    __SET(n_1, 0);
+    __SET(n_2, 0);
+    __SET(n_3, 0);
+    __SET(n_4, 0);
+    __SET(n_5, 0);
+    __SET(n_6, 0);
+    return t_select_func;
 }
 
-void t_ntbl_bitcount() {
-    printf("t_ntbl_bitcount task \n");
-    unsigned int __cry = seed;
-
-    n_3 += bits[ (int) (__cry & 0x0000000FUL)       ] +
-        bits[ (int)((__cry & 0x000000F0UL) >> 4) ] +
-        bits[ (int)((__cry & 0x00000F00UL) >> 8) ] +
-        bits[ (int)((__cry & 0x0000F000UL) >> 12)] +
-        bits[ (int)((__cry & 0x000F0000UL) >> 16)] +
-        bits[ (int)((__cry & 0x00F00000UL) >> 20)] +
-        bits[ (int)((__cry & 0x0F000000UL) >> 24)] +
-        bits[ (int)((__cry & 0xF0000000UL) >> 28)];
-    seed = seed + plus;
-    /*
-    printf("%d, ", (int) (__cry & 0x0000000FUL));
-    printf("%d, ", (int)((__cry & 0x000000F0UL) >> 4));
-    printf("%d, ", (int)((__cry & 0x00000F00UL) >> 8));
-    printf("%d, ", (int)((__cry & 0x0000F000UL) >> 12));
-    printf("%d, ", (int)((__cry & 0x000F0000UL) >> 16));
-    printf("%d, ", (int)((__cry & 0x00F00000UL) >> 20));
-    printf("%d, ", (int)((__cry & 0x0F000000UL) >> 24));
-    printf("%d \n", (int)((__cry & 0xF0000000UL) >> 28));
-    */
-    //printf("** n_3 %d \n", n_3);
-    iteration++;
-
-    if (iteration < ITER) {
-      t_ntbl_bitcount();
-    }
-}
-
-
-void t_BW_btbl_bitcount() {
-    printf("t_BW_btbl_bitcount task \n");
-
-    union {
-        unsigned char ch[6];
-        long y;
-    } U;
-
-    U.y = seed;
-
-    int aa = U.ch[0] + U.ch[1] + U.ch[2] + U.ch[3]; 
-    n_4 += bits[ U.ch[0] ] + bits[ U.ch[1] ] + bits[ U.ch[2] ] + bits[ U.ch[3] ];
-
-    seed = seed + plus;
-    //printf("** n_4 %d \n", n_4);
-    //printf("%d, %d, %d, %d \n", seed << 24 >> 24, seed << 16 >> 24,  seed << 8 >> 24, seed >> 24);
-    //printf("%d, %d, %d, %d \n", U.ch[0], U.ch[1], U.ch[2], U.ch[3]);
-    iteration++;
-
-    if (iteration < ITER) {
-      t_BW_btbl_bitcount();
-    }
-}
-
-void t_AR_btbl_bitcount() {
-    printf("t_AR_btbl_bitcount task \n");
-
-    unsigned char * Ptr = (unsigned char *) &seed ;
-
-    //printf("%d, ", *Ptr);
-    n_5  += bits[ *Ptr++ ];
-    //printf("%d, ", *Ptr);
-    n_5 += bits[ *Ptr++ ];
-    //printf("%d, ", *Ptr);
-    n_5 += bits[ *Ptr++ ];
-    //printf("%d \n", *Ptr);
-    n_5 += bits[ *Ptr ];
-
-    seed = seed + plus;
-    //printf("** n_5 %d \n", n_5);
-    iteration++;
-
-    if (iteration < ITER) {
-      t_AR_btbl_bitcount();
-    }
-}
-
-void t_bit_shifter() {
-    printf("t_bit_shifter task \n");
-
-    unsigned i, nn;
-    unsigned int tmp_seed = seed;
-    for (i = nn = 0; tmp_seed && (i < (sizeof(long) * CHAR_BIT)); ++i, tmp_seed >>= 1)
-        nn += (unsigned)(tmp_seed & 1L);
-    n_6 += nn;
-
-    seed = seed + plus;
-    //printf("** n_6 %d \n", n_6);
-    iteration++;
-
-    if (iteration < ITER) {
-      t_bit_shifter();
-    }
-}
-
-void t_end() {
-  printf("***** %d \n", n_0);
-  printf("***** %d \n", n_1);
-  printf("***** %d \n", n_2);
-  printf("***** %d \n", n_3);
-  printf("***** %d \n", n_4);
-  printf("***** %d \n", n_5);
-  printf("***** %d \n", n_6);
-}
-
-
-int main(void) {
-    printf("\n\n");
-    n_0 = n_1 = n_2 = n_3 = n_4 = n_5 = n_6 = 0;
-    
-  while(1) {
-    iteration = 0;
-    if (function == 0) {
-      function++;
-      t_bit_count();
-    } else if (function == 1){
-      function++;
-      t_bitcount();
-    } else if (function == 2){
-      function++;
-      t_ntbl_bitcnt();
-    } else if (function == 3){
-      function++;
-      t_ntbl_bitcount();
-    } else if (function == 4){
-      function++;
-      t_BW_btbl_bitcount();
-    } else if (function == 5){
-      function++;
-      t_AR_btbl_bitcount();
-    } else if (function == 6){
-      function++;
-      t_bit_shifter();
+TASK(t_select_func) {
+    uint32_t _function = __GET(function);
+    __SET(iteration, 0);
+    if(_function == 0) {
+        __SET(function, _function + 1);
+        return t_bit_count;
+    } else if (_function == 1) {
+        __SET(function, _function + 1);
+        return t_bitcount;
+    } else if (_function == 2) {
+        __SET(function, _function + 1);
+        return t_ntbl_bitcnt;
+    } else if (_function == 3) {
+        __SET(function, _function + 1);
+        return t_ntbl_bitcount;
+    } else if (_function == 4) {
+        __SET(function, _function + 1);
+        return t_BW_btbl_bitcount;
+    } else if (_function == 5) {
+        __SET(function, _function + 1);
+        return t_AR_btbl_bitcount;
+    } else if (_function == 6) {
+        __SET(function, _function + 1);
+        return t_bit_shifter;
     } else {
-      function++;
-      t_end();
-      break;
+        __SET(function, _function + 1);
+        return t_end;
     }
-  }
-
-    return 0;
 }
 
+TASK(t_bit_count) {
+    uint32_t _seed = __GET(seed);
+    uint32_t _n_0 = __GET(n_0);
+    uint32_t _iteration = __GET(iteration);
+    uint32_t tmp_seed = _seed;
+    uint32_t temp = 0;
+    _seed = tmp_seed + plus;
+    if(tmp_seed > 0) {
+        temp = temp + 1;
+        tmp_seed = tmp_seed & (tmp_seed - 1);
+        
+        while(tmp_seed != 0) {
+            temp = temp + 1;
+            tmp_seed = tmp_seed & (tmp_seed - 1);
+        }
+    } 
+    _iteration = _iteration + 1;
+    __SET(seed, _seed);
+    __SET(n_0, _n_0 + temp);
+    __SET(iteration, _iteration);
+    if(_iteration < ITER) {
+        return t_bit_count;
+    } else {
+        return t_select_func;
+    }
+}
 
+TASK(t_bitcount) {
+    uint32_t _seed = __GET(seed);
+    uint32_t _n_1 = __GET(n_1);
+    uint32_t _iteration = __GET(iteration);
+    uint32_t tmp_seed = _seed;
+    _seed = tmp_seed + plus;
+    tmp_seed = ((tmp_seed & ) >> 1) + (tmp_seed & );
+    tmp_seed = ((tmp_seed & ) >> 2) + (tmp_seed & );
+    tmp_seed = ((tmp_seed & ) >> 4) + (tmp_seed & );
+    tmp_seed = ((tmp_seed & ) >> 8) + (tmp_seed & );
+    tmp_seed = ((tmp_seed & ) >> 16) + (tmp_seed & );
+    _iteration = _iteration + 1;
+    __SET(seed, _seed);
+    __SET(n_1, _n_1 + tmp_seed);
+    __SET(iteration, _iteration);
+    if(_iteration < ITER) {
+        return t_bitcount;
+    } else {
+        return t_select_func;
+    }
+}
+
+TASK(t_ntbl_bitcnt) {
+    uint32_t _seed = __GET(seed);
+    uint32_t _n_2 = __GET(n_2);
+    uint32_t _iteration = __GET(iteration);
+    uint32_t tmp_seed = _seed;
+    uint32_t function_result = non_recursive_cnt(tmp_seed);
+    _seed = tmp_seed + plus;
+    _iteration = _iteration + 1;
+    __SET(seed, _seed);
+    __SET(n_2, _n_2 + function_result);
+    __SET(iteration, _iteration);
+    if(_iteration < ITER) {
+        return t_ntbl_bitcnt;
+    } else {
+        return t_select_func;
+    }
+}
+
+TASK(t_ntbl_bitcount) {
+    uint32_t _seed = __GET(seed);
+    uint32_t _n_3 = __GET(n_3);
+    uint32_t _iteration = __GET(iteration);
+    uint32_t tmp_seed = _seed;
+    uint32_t __cry = _seed;
+    uint32_t index = __cry & ;
+    _n_3 = _n_3 + bits[ index ];
+    index = (__cry & ) >> 4;
+    _n_3 = _n_3 + bits[ index ];
+    index = (__cry & ) >> 8;
+    _n_3 = _n_3 + bits[ index ];
+    index = (__cry & ) >> 12;
+    _n_3 = _n_3 + bits[ index ];
+    index = (__cry & ) >> 16;
+    _n_3 = _n_3 + bits[ index ];
+    index = (__cry & ) >> 20;
+    _n_3 = _n_3 + bits[ index ];
+    index = (__cry & ) >> 24;
+    _n_3 = _n_3 + bits[ index ];
+    index = (__cry & ) >> 28;
+    _n_3 = _n_3 + bits[ index ];
+    _seed = tmp_seed + plus;
+    _iteration = _iteration + 1;
+    __SET(n_3, _n_3);
+    __SET(seed, _seed);
+    __SET(iteration, _iteration);
+    if(_iteration < ITER) {
+        return t_ntbl_bitcount;
+    } else {
+        return t_select_func;
+    }
+}
+
+TASK(t_BW_btbl_bitcount) {
+    uint32_t _seed = __GET(seed);
+    uint32_t _n_4 = __GET(n_4);
+    uint32_t _iteration = __GET(iteration);
+    uint32_t index = _seed << 24 >> 24;
+    _n_4 = _n_4 + bits[ index ];
+    index = _seed << 16 >> 24;
+    _n_4 = _n_4 + bits[ index ];
+    index = _seed << 8 >> 24;
+    _n_4 = _n_4 + bits[ index ];
+    index = _seed >> 24;
+    _n_4 = _n_4 + bits[ index ];
+    _seed = _seed + plus;
+    _iteration = _iteration + 1;
+    __SET(n_4, _n_4);
+    __SET(seed, _seed);
+    __SET(iteration, _iteration);
+    if(_iteration < ITER) {
+        return t_BW_btbl_bitcount;
+    } else {
+        return t_select_func;
+    }
+}
+
+TASK(t_AR_btbl_bitcount) {
+    uint32_t _seed = __GET(seed);
+    uint32_t _n_5 = __GET(n_5);
+    uint32_t _iteration = __GET(iteration);
+    uint32_t index = _seed << 24 >> 24;
+    _n_5 = _n_5 + bits[ index ];
+    index = _seed << 16 >> 24;
+    _n_5 = _n_5 + bits[ index ];
+    index = _seed << 8 >> 24;
+    _n_5 = _n_5 + bits[ index ];
+    index = _seed >> 24;
+    _n_5 = _n_5 + bits[ index ];
+    _seed = _seed + plus;
+    _iteration = _iteration + 1;
+    __SET(n_5, _n_5);
+    __SET(seed, _seed);
+    __SET(iteration, _iteration);
+    if(_iteration < ITER) {
+        return t_AR_btbl_bitcount;
+    } else {
+        return t_select_func;
+    }
+}
+
+TASK(t_bit_shifter) {
+    uint32_t _seed = __GET(seed);
+    uint32_t _n_6 = __GET(n_6);
+    uint32_t _iteration = __GET(iteration);
+    uint32_t i = 0;
+    uint32_t nn;
+    uint32_t tmp_seed = _seed;
+    
+    for (nn = 0; 0 < tmp_seed; tmp_seed = tmp_seed >> 1) {
+        i = i + 1;
+        if(i < 8 * CHAR_BIT) {
+            nn = nn + (tmp_seed & 1);
+        } else {
+            break;
+        }
+    }
+    _n_6 = _n_6 + nn;
+    _seed = _seed + plus;
+    _iteration = _iteration + 1;
+    __SET(n_6, _n_6);
+    __SET(seed, _seed);
+    __SET(iteration, _iteration);
+    if(_iteration < ITER) {
+        return t_bit_shifter;
+    } else {
+        return t_select_func;
+    }
+}
+
+TASK(t_end) {
+    uint32_t _pinCont = __GET(pinCont);
+    __SET(pinCont, 0);
+    return NULL;
+}
 
---
+++ Apps/cem.c
@@ -0,0 +1,285 @@
+// This file is part of InK.
+
+/*
+ * InK is free software: you ca    n redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published *
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "ink.h"
+#include <stdlib.h>
+
+
+// Define constant persistent variables that are stored in non-volatile memory.
+uint32_t NIL = 0;
+uint32_t DICT_SIZE = 512;
+uint32_t BLOCK_SIZE = 64;
+uint32_t NUM_LETTERS_IN_SAMPLE = 2;
+uint32_t LETTER_MASK = ;
+uint32_t LETTER_SIZE_BITS = 8;
+uint32_t NUM_LETTERS = (LETTER_MASK + 1);
+
+
+// Define task-shared persistent variables.
+__shared(
+    uint32_t pinCont;
+    uint32_t v_letter_idx;
+    uint32_t v_letter;
+    uint32_t v_prev_sample;
+    uint32_t v_out_len;
+    uint32_t v_node_count;
+    uint32_t v_sample;
+    uint32_t v_sample_count;
+    uint32_t v_sibling;
+    uint32_t v_child;
+    uint32_t v_parent;
+    uint32_t v_parent_next;
+    uint32_t v_parent_node_letter;
+    uint32_t v_parent_node_sibling;
+    uint32_t v_parent_node_child;
+    uint32_t v_sibling_node_letter;
+    uint32_t v_sibling_node_sibling;
+    uint32_t v_sibling_node_child;
+    uint32_t v_symbol;
+    uint32_t v_compressed_data_letter[ BLOCK_SIZE ];
+    uint32_t v_compressed_data_sibling[ BLOCK_SIZE ];
+    uint32_t v_compressed_data_child[ BLOCK_SIZE ];
+    uint32_t v_dict_letter[ DICT_SIZE ];
+    uint32_t v_dict_sibling[ DICT_SIZE ];
+    uint32_t v_dict_child[ DICT_SIZE ];
+)
+
+
+// Declare tasks that will be implemented
+ENTRY_TASK(t_init);
+TASK(task_init_dict);
+TASK(task_sample);
+TASK(task_measure_temp);
+TASK(task_letterize);
+TASK(task_compress);
+TASK(task_find_sibling);
+TASK(task_add_node);
+TASK(task_add_insert);
+TASK(task_append_compressed);
+TASK(task_print);
+TASK(task_done);
+
+
+// Called at the very first boot
+void thread1_init(){
+    // create a thread with priority 15 and entry task t_init
+    __CREATE(15, t_init);
+    __SIGNAL(15);
+}
+
+
+// Implementation of all tasks that are declared above
+ENTRY_TASK(t_init) {
+    __SET(pinCont, 1);
+    __SET(v_parent_next, 0);
+    __SET(v_out_len, 0);
+    __SET(v_letter, 0);
+    __SET(v_prev_sample, 0);
+    __SET(v_letter_idx, 0);
+    __SET(v_sample_count, 1);
+    return task_init_dict;
+}
+
+TASK(task_init_dict) {
+    uint32_t _v_letter = __GET(v_letter);
+    v_dict_letter[ _v_letter ] = _v_letter;
+    v_dict_sibling[ _v_letter ] = NIL;
+    v_dict_child[ _v_letter ] = NIL;
+    __SET(v_letter, _v_letter + 1);
+    if(_v_letter < NUM_LETTERS) {
+        return task_init_dict;
+    } else {
+        __SET(v_node_count, NUM_LETTERS);
+        return task_sample;
+    }
+}
+
+TASK(task_sample) {
+    uint32_t _v_letter_idx = __GET(v_letter_idx);
+    uint32_t _next_v_letter_idx = _v_letter_idx + 1;
+    if(_next_v_letter_idx == NUM_LETTERS_IN_SAMPLE) {
+        _next_v_letter_idx = 0;
+    } 
+    if(_v_letter_idx == 0) {
+        __SET(v_letter_idx, _next_v_letter_idx);
+        return task_measure_temp;
+    } else {
+        __SET(v_letter_idx, _next_v_letter_idx);
+        return task_letterize;
+    }
+}
+
+TASK(task_measure_temp) {
+    uint32_t _v_prev_sample = __GET(v_prev_sample);
+    uint32_t sample = acquire_sample(_v_prev_sample);
+    _v_prev_sample = sample;
+    __SET(v_prev_sample, _v_prev_sample);
+    __SET(v_sample, sample);
+    return task_letterize;
+}
+
+TASK(task_letterize) {
+    uint32_t _v_letter_idx = __GET(v_letter_idx);
+    uint32_t letter_shift;
+    uint32_t letter;
+    if(_v_letter_idx == 0) {
+        _v_letter_idx = NUM_LETTERS_IN_SAMPLE;
+    } else {
+        _v_letter_idx = _v_letter_idx - 1;
+    }
+    letter_shift = LETTER_SIZE_BITS * _v_letter_idx;
+    letter = (__GET(v_sample) & (LETTER_MASK << letter_shift)) >> letter_shift;
+    __SET(v_letter, letter);
+    return task_compress;
+}
+
+TASK(task_compress) {
+    uint32_t parent = __GET(v_parent_next);
+    uint32_t __cry;
+    __cry = __GET(v_dict_child)[ parent ];
+    __SET(v_sibling, __cry);
+    __SET(v_parent_node_child, __cry);
+    __SET(v_child, __cry);
+    __cry = __GET(v_dict_letter)[ parent ];
+    __SET(v_parent_node_letter, __cry);
+    __cry = __GET(v_dict_sibling)[ parent ];
+    __SET(v_parent_node_sibling, __cry);
+    __SET(v_parent, parent);
+    __SET(v_sample_count, __GET(v_sample_count) + 1);
+    return task_find_sibling;
+}
+
+TASK(task_find_sibling) {
+    uint32_t i = __GET(v_sibling);
+    uint32_t __cry;
+    uint32_t starting_node_idx;
+    if(i != NIL) {
+        __cry = __GET(v_letter);
+        if(__GET(v_dict_letter)[ i ] != __cry) {
+            __cry = __GET(v_sibling);
+            __SET(v_parent_next, __cry);
+            return task_letterize;
+        } else if (__GET(v_dict_sibling)[ i ] != 0) {
+            __cry = __GET(v_dict_sibling)[ i ];
+            __SET(v_sibling, __cry);
+            return task_find_sibling;
+        } else {
+            starting_node_idx = __GET(v_letter);
+            __SET(v_parent_next, starting_node_idx);
+            if(__GET(v_child) == NIL) {
+                return task_add_insert;
+            } else {
+                return task_add_node;
+            }
+        }
+    } else {
+        starting_node_idx = __GET(v_letter);
+        __SET(v_parent_next, starting_node_idx);
+        if(__GET(v_child) == NIL) {
+            return task_add_insert;
+        } else {
+            return task_add_node;
+        }
+    }
+}
+
+TASK(task_add_node) {
+    uint32_t i = __GET(v_sibling);
+    uint32_t next_sibling;
+    uint32_t __cry;
+    if(__GET(v_dict_sibling)[ i ] != NIL) {
+        next_sibling = __GET(v_dict_sibling)[ i ];
+        __SET(v_sibling, next_sibling);
+        return task_add_node;
+    } else {
+        __cry = __GET(v_dict_letter)[ i ];
+        __SET(v_sibling_node_letter, __cry);
+        __cry = __GET(v_dict_sibling)[ i ];
+        __SET(v_sibling_node_sibling, __cry);
+        __cry = __GET(v_dict_child)[ i ];
+        __SET(v_sibling_node_child, __cry);
+        return task_add_insert;
+    }
+}
+
+TASK(task_add_insert) {
+    uint32_t child;
+    uint32_t __cry;
+    uint32_t i;
+    uint32_t last_sibling;
+    if(__GET(v_node_count) == DICT_SIZE) {
+        
+        while(1 == 1) {
+        }
+    } 
+    child = __GET(v_node_count);
+    if(__GET(v_parent_node_child) == NIL) {
+        __SET(v_parent_node_child, child);
+        i = __GET(v_parent);
+        __cry = __GET(v_parent_node_letter);
+        v_dict_letter[ i ] = __cry;
+        __cry = __GET(v_parent_node_sibling);
+        v_dict_sibling[ i ] = __cry;
+        __cry = __GET(v_parent_node_child);
+        v_dict_child[ i ] = __cry;
+    } else {
+        last_sibling = __GET(v_sibling);
+        __SET(v_sibling_node_sibling, child);
+        __cry = __GET(v_sibling_node_letter);
+        v_dict_letter[ last_sibling ] = __cry;
+        __cry = __GET(v_sibling_node_sibling);
+        v_dict_sibling[ last_sibling ] = __cry;
+        __cry = __GET(v_sibling_node_child);
+        v_dict_child[ last_sibling ] = __cry;
+    }
+    __cry = __GET(v_letter);
+    v_dict_letter[ child ] = __cry;
+    v_dict_sibling[ child ] = NIL;
+    v_dict_child[ child ] = NIL;
+    __cry = __GET(v_parent);
+    __SET(v_symbol, __cry);
+    __SET(v_node_count, __GET(v_node_count) + 1);
+    return task_append_compressed;
+}
+
+TASK(task_append_compressed) {
+    uint32_t __cry = __GET(v_symbol);
+    uint32_t i = __GET(v_out_len);
+    v_compressed_data_letter[ i ] = __cry;
+    __SET(v_out_len, __GET(v_out_len) + 1);
+    if(__GET(v_out_len) == BLOCK_SIZE) {
+        return task_print;
+    } else {
+        return task_sample;
+    }
+}
+
+TASK(task_print) {
+    uint32_t i;
+    uint32_t index;
+    
+    for (i = 0; i < BLOCK_SIZE; i = i + 1) {
+        index = __GET(v_compressed_data_letter)[ i ];
+    }
+    return task_done;
+}
+
+TASK(task_done) {
+    return NULL;
+}
+
